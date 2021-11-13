c036dceaae6450c9965b55c810f161305805bec7
Haritha <ext-harithalaxmi.chandrasekaran@thoughtworks.com>
2021-11-13 22:17:38 +0530

    add file to calculate

---
+++ Summation.java
@@ -0,0 +1,14 @@
+public class Summation
+{
+    //function to function summation of numbers 1-100
+    public static void main(String[] args)
+    {
+          int j=0;
+          for(int i=1;i<=100;i++)
+          {
+              j=j+i;
+          }
+        System.out.println("Summation of numbers is "+j);
+
+    }
+}
